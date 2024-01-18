610edb98b2b4911473b01cc5cfd565236ae23a6d
AJITHKUMAR V <84790549+Akpro865@users.noreply.github.com>
2024-01-18 13:03:22 +0530

    Create Thankyou.jsx
    Thank you page added

---
+++ src/comps/Thankyou.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import Footer from './Footer'
+
+function Thankyou(){
+	return (
+		<>
+		   <Header />
+		   <div className='sm:px-10 lg:px-28 px-3'>
+		     <h3>Thank You</h3>
+		   </div>
+		   <Footer />
+		</>
+	)
+}
+
+export default Home
