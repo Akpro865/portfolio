70f6f0c29c2c2d92b7ffe141fda9a15a11359a2a
AJITHKUMAR V <84790549+Akpro865@users.noreply.github.com>
2024-01-18 14:10:10 +0530

    link with navigate

--- src/comps/Thankyou.jsx
+++ src/comps/Thankyou.jsx
@@ -5,8 +5,8 @@ function Thankyou(){
 	return (
 		<>
 		   <Header />
-		   <div className='sm:px-10 lg:px-28 px-3'>
-		     <h3>Thank You</h3>
+		   <div className='sm:px-10 lg:px-28 px-3 h-3/4 flex justify-center items-center'>
+		     <h3 className='text-xl'>Thank You</h3>
 		   </div>
 		   <Footer />
 		</>
